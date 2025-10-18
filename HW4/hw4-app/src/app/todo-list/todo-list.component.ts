ee16917bd62686ebcf444c92afa50f9e4d721f0f
sabrsaqta <1sabrsaqta1@gmail.com>
2025-10-18 13:04:09 +0500

    added hw5

--- HW4/hw4-app/src/app/todo-list/todo-list.component.ts
+++ HW4/hw4-app/src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TodoService } from '../todo.service';
 import { CommonModule } from '@angular/common';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo-list',
@@ -9,18 +11,41 @@ import { CommonModule } from '@angular/common';
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, OnDestroy {
   todos: any[] = [];
   loaded = false;
 
+  private searchSubject = new Subject<string>(); //отслеживания ввода
+
   constructor(private todoService: TodoService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.searchSubject.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+
+      //запрос на сервер/отмена предыдущих при новом
+      switchMap(searchTerm => this.todoService.searchTodos(searchTerm))
+    ).subscribe(data => {
+      this.todos = data;
+      this.loaded = true;
+    });
+  }
+
+  onSearch(searchTerm: string): void{
+    this.searchSubject.next(searchTerm);
+  }
+
+
 
   loadTodos() {
     this.todoService.getTodos().subscribe(data =>{
       this.todos = data.slice(0,10);
       this.loaded = true;
     })
+  }
+
+  ngOnDestroy(): void {
+      this.searchSubject.complete();
   }
 }
--- HW4/hw4-app/src/app/todo.service.ts
+++ HW4/hw4-app/src/app/todo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,16 @@ export class TodoService {
 
   getTodos(): Observable<any[]>{
     return this.http.get<any[]>(this.apiUrl);
+  }
+
+
+  searchTodos(term: string): Observable<any[]>{
+    if(!term.trim()){
+      return of([]);
+    }
+
+    const searchUrl = `${this.apiUrl}?q=${term}`;
+    return this.http.get<any[]>(searchUrl);
   }
 }
 
